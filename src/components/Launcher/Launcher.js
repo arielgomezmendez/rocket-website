a544de022ead3b2cd0e8555876e6db31cd30be85
Ariel <arielgomezmendez94@gmail.com>
2023-03-11 23:16:52 -0500

    Create the Launcher component
    Create structure and add styles

---
+++ src/components/Launcher/Launcher.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Launcher = () => {
+  return (
+    <div>
+      <div class="launcher">
+        <div class="launcher-details">
+          <p class="text-title">Card title</p>
+          <p class="text-body">Here are the details of the card</p>
+        </div>
+        <button class="launcher-button">More info</button>
+      </div>
+    </div>
+  );
+};
+
+export default Launcher;
