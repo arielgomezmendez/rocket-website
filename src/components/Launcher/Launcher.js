e11151bfd2c8034a40be514855ba9cfadcc628a4
Ariel <arielgomezmendez94@gmail.com>
2023-03-12 11:20:48 -0400

    Renderize all the cards with launchers with launchers name

--- src/components/Launcher/Launcher.js
+++ src/components/Launcher/Launcher.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./Launcher.css";
 
-const Launcher = () => {
+const Launcher = ({ launchers }) => {
   return (
-    <div className="launcher">
-      <div className="launcher-details">
-        <p className="text-title">Card title</p>
-        <p className="text-body">Here are the details of the card</p>
-      </div>
-      <button className="launcher-button">More info</button>
-    </div>
+    <Fragment>
+      {launchers.map((launcher) => (
+        <div className="launcher" key={launcher.id}>
+          <div className="launcher-details">
+            <p className="text-title">{launcher.name}</p>
+            <p className="text-body">Here are the details of the card</p>
+          </div>
+          <button className="launcher-button">More info</button>
+        </div>
+      ))}
+    </Fragment>
   );
 };
 
 export default Launcher;
+{
+  /*
+{launchers.map((l, i) => l[i].name)}
+ */
+}
