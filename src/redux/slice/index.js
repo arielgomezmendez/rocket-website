26d09387de6988b03b8a44b31846e49bb0d1a197
Ariel <arielgomezmendez94@gmail.com>
2023-03-16 07:47:17 -0400

    Create the reducers of Redux

---
+++ src/redux/slice/index.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  launches: null,
+};
+
+export const launchersSlice = createSlice({
+  name: "launchers",
+  initialState,
+  reducers: {
+    setLaunches: (state, action) => {
+      state.launches = action.payload;
+    },
+  },
+});
+
+export const { setLaunches } = launchersSlice.actions;
+export default launchersSlice.reducer;
