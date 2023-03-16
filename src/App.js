e2a6ffca80c8dcb1d5d79e46ed4be5e3b3d2fdb0
Ariel <arielgomezmendez94@gmail.com>
2023-03-16 07:50:19 -0400

    Wrap the App component in the Provider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import "./App.css";
 import "./mobile.css";
 import LaunchInfo from "./components/LaunchInfo/LaunchInfo";
 import Launches from "./components/Launches/Launches";
+//React-router
 import { Route, Routes } from "react-router";
+//Redux-toolkit
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Launches />} />
-        <Route path="/:id" element={<LaunchInfo />} />
-      </Routes>
+      <Provider>
+        <Routes>
+          <Route path="/" element={<Launches />} />
+          <Route path="/:id" element={<LaunchInfo />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
